94287db27866a2a1812186fa6ddf47365da17b09
John Adibe <johnadibe123@gmail.com>
2023-03-01 10:22:44 -0800

    Add bookslice and categoriesslice files to redux folder

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  categories: [],
+};
+
+export const categorySlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+    checkStatus: () => ({
+      categories: 'Under Construction',
+    }),
+  },
+});
+
+export const { checkStatus } = categorySlice.actions;
+
+export default categorySlice.reducer;
